74ba6cfa13ddb0d260caec30ce1b04365b000c19
Shehan Rangana <shehanr@swivelgroup.com.au>
2023-06-23 17:08:45 +0530

    logs write permission validation added.

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import winstonLogger from './utils/winston.logger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: winstonLogger,
+    logger: winstonLogger(),
   });
 
   app.setGlobalPrefix('api');
--- src/utils/winston.logger.ts
+++ src/utils/winston.logger.ts
@@ -1,39 +1,57 @@
+import * as fs from 'fs';
 import { WinstonModule } from 'nest-winston';
 import { format, transports } from 'winston';
 import 'winston-daily-rotate-file';
 
 const timeFormat = 'YYYY-MM-DD, HH:mm:ss';
 
-export default WinstonModule.createLogger({
-  transports: [
-    // Create daily rotate file for errors. %DATE will be substituted with the current date.
-    new transports.DailyRotateFile({
-      filename: `logs/%DATE%-error.log`,
-      level: 'error',
-      format: format.combine(format.timestamp({ format: timeFormat }), format.json()),
-      datePattern: 'YYYY-MM-DD',
-      maxFiles: '30d',
-    }),
+const winstonLogger = () => {
+  /**
+   * This additional permission check accounts for serverless functions that may not have write permissions.
+   */
+  let errorFilename = 'logs/%DATE%-error.log';
+  let combinedFilename = 'logs/%DATE%-combined.log';
 
-    // Create daily rotate file for all the logs
-    new transports.DailyRotateFile({
-      filename: `logs/%DATE%-combined.log`,
-      format: format.combine(format.timestamp({ format: timeFormat }), format.json()),
-      datePattern: 'YYYY-MM-DD',
-      maxFiles: '20d',
-    }),
+  fs.access('logs', fs.constants.W_OK, (err) => {
+    if (err) {
+      errorFilename = '/tmp/' + errorFilename;
+      combinedFilename = '/tmp/' + combinedFilename;
+    }
+  });
 
-    // Enable console logs
-    new transports.Console({
-      format: format.combine(
-        format.colorize({ all: true }),
-        format.cli(),
-        format.splat(),
-        format.timestamp({ format: timeFormat }),
-        format.printf((info) => {
-          return `${info.timestamp} ${info.level} [${info.context}] ${info.message}`;
-        }),
-      ),
-    }),
-  ],
-});
+  return WinstonModule.createLogger({
+    transports: [
+      // Create daily rotate file for errors. %DATE will be substituted with the current date.
+      new transports.DailyRotateFile({
+        filename: errorFilename,
+        level: 'error',
+        format: format.combine(format.timestamp({ format: timeFormat }), format.json()),
+        datePattern: 'YYYY-MM-DD',
+        maxFiles: '30d',
+      }),
+
+      // Create daily rotate file for all the logs
+      new transports.DailyRotateFile({
+        filename: combinedFilename,
+        format: format.combine(format.timestamp({ format: timeFormat }), format.json()),
+        datePattern: 'YYYY-MM-DD',
+        maxFiles: '20d',
+      }),
+
+      // Enable console logs
+      new transports.Console({
+        format: format.combine(
+          format.colorize({ all: true }),
+          format.cli(),
+          format.splat(),
+          format.timestamp({ format: timeFormat }),
+          format.printf((info) => {
+            return `${info.timestamp} ${info.level} [${info.context}] ${info.message}`;
+          }),
+        ),
+      }),
+    ],
+  });
+};
+
+export default winstonLogger;
